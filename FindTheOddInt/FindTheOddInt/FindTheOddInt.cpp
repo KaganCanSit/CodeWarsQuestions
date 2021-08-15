1add21d68c5e778ffe102cda9020f0a16c11ee6e
Kağan Can Şit <kagancansit@hotmail.com>
2021-08-16 01:02:56 +0300

    v001

---
+++ FindTheOddInt/FindTheOddInt/FindTheOddInt.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>,
+#include <vector>
+using namespace std;
+
+int findOdd(const vector<int>& numbers);
+
+int main(void)
+{
+    cout << "Hello World!\n";
+}
+
+int findOdd(const vector<int>& numbers)
+{
+
+}
+//20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5
