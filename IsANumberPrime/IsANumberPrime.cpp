ffe7d9648f51b4a711500b5aea05b6a5c8356feb
Kağan Can Şit <kagancansit@hotmail.com>
2021-08-27 13:12:02 +0300

    v001

---
+++ IsANumberPrime/IsANumberPrime.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPrime(int num);
+
+int main()
+{
+     cout <<  isPrime(2);
+}
+
+bool isPrime(int num)
+{
+    if (num < 2)
+        return false;
+
+    for (int i = 3; i < sqrt(num) + 1; i++)
+        if (num % i == 0)
+            return false;
+
+    return true;
+}
